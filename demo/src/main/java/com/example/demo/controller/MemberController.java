1710570c41e843cb255ab245f099d824d5aa7395
hayoung <hayohio@gmail.com>
2026-01-09 16:24:03 +0900

    Auto commit at Fri Jan  9 16:24:03     2026

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -29,7 +29,7 @@ public class MemberController {
     @GetMapping("/update/{id}")
     public String updateForm(@PathVariable int id, Model model){
         log.info("id : " + id);
-        memberService
+        MemberDTO
         return "member/updateForm";
     }
 
