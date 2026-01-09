cdcfd10a74dcf91110b0132b6e798e6bf1d129d7
hayoung <hayohio@gmail.com>
2026-01-09 15:50:00 +0900

    Auto commit by hayoung at Fri Jan  9 15:50:00     2026

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,8 +1,16 @@
 package com.example.demo.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
+@RequestMapping("/basic")
+@Log4j2
+@RequiredArgsConstructor
 public class HelloController {
 
     @GetMapping("/literal")
