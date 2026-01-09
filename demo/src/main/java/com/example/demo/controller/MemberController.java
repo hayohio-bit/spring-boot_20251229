d083fc6da41c8d655ad45254938602fe38b47a3b
hayoung <hayohio@gmail.com>
2026-01-09 16:42:17 +0900

    Auto commit at Fri Jan  9 16:42:17     2026

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -37,6 +37,7 @@ public class MemberController {
     @PostMapping("/update")
     public String updatePost(MemberDTO memberDTO){
         memberService.update(memberDTO);
+        return null;
     }
 
 }
