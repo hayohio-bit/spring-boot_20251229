4c06baa33f5a83373d705cdb5ebdaea884690e7f
hayoung <hayohio@gmail.com>
2026-01-09 16:42:01 +0900

    Auto commit at Fri Jan  9 16:42:01     2026

--- demo/src/main/java/com/example/demo/controller/MemberController.java
+++ demo/src/main/java/com/example/demo/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -31,6 +32,12 @@ public class MemberController {
         MemberDTO memberDTO = memberService.findById(id);
         model.addAttribute("member", memberDTO);
         return "member/updateForm";
+    }
+
+    @PostMapping("/update")
+    public String updatePost(MemberDTO memberDTO){
+
+        
     }
 
 }
