db2a5f1533f24664219181c86d02f4db9a3035b8
hayoung <hayohio@gmail.com>
2026-01-14 16:34:02 +0900

    Auto: 2026-01-14 16:34

--- jpa-crud/src/main/java/com/example/myapp/service/UserService.java
+++ jpa-crud/src/main/java/com/example/myapp/service/UserService.java
@@ -1,9 +1,13 @@
 package com.example.myapp.service;
 
+import com.example.myapp.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service        // 서비스 레이어 빈으로 등록
 @Transactional  // 기본적으로 메서드들을 트랜잭션 안에서 실행
 public class UserService {
+
+    private final UserRepository userRepository;
+    // User 
 }
