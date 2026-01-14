8dc8b665b506ce0f65667bc19df67b600491bf10
hayoung <hayohio@gmail.com>
2026-01-14 16:28:19 +0900

    Auto: 2026-01-14 16:28

--- jpa-crud/src/main/java/com/example/myapp/repository/UserRepository.java
+++ jpa-crud/src/main/java/com/example/myapp/repository/UserRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 // User 엔티티를 DB에 CRUD 할 수 있게 해주는 JPA 리포지토리 인터페이스
 public interface UserRepository extends JpaRepository<User, Long> {
     // 기본 CRUD, 페이징 메서드 들은 JpaRepository가 이미 제공함
-    // JpaRepository<User, Long>
+    // JpaRepository<User, Long> 에서 User는 domain.User 이고 Long은 User.
 }
