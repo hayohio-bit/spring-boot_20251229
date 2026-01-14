d77a503399467f0a2f0343f113d0ccf3cd9529ad
hayoung <hayohio@gmail.com>
2026-01-14 16:24:18 +0900

    Auto: 2026-01-14 16:24

--- jpa-crud/src/main/java/com/example/myapp/repository/UserRepository.java
+++ jpa-crud/src/main/java/com/example/myapp/repository/UserRepository.java
@@ -1,4 +1,6 @@
 package com.example.myapp.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository {
 }
