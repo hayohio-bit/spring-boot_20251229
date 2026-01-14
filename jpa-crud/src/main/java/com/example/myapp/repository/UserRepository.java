f458d57082abd04e2bce81a09e347769840b2b63
hayoung <hayohio@gmail.com>
2026-01-14 16:25:41 +0900

    Auto: 2026-01-14 16:25

--- jpa-crud/src/main/java/com/example/myapp/repository/UserRepository.java
+++ jpa-crud/src/main/java/com/example/myapp/repository/UserRepository.java
@@ -2,6 +2,6 @@ package com.example.myapp.repository;
 
 import com.example.myapp.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// User 엔티티를 DB에 CRUD 할 수 있게 해주는 JPA 리포지토리 인터페이스
 public interface UserRepository extends JpaRepository<User, Long> {
 }
