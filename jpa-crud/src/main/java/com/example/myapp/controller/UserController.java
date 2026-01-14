969489ecce5f36bfbc841eceb55697d4f2833584
hayoung <hayohio@gmail.com>
2026-01-14 17:28:03 +0900

    Auto: 2026-01-14 17:28

--- jpa-crud/src/main/java/com/example/myapp/controller/UserController.java
+++ jpa-crud/src/main/java/com/example/myapp/controller/UserController.java
@@ -42,5 +42,11 @@ public class UserController {
         );
     }
 
+    // ==== 4. 회원 삭제 ====
+    @DeleteMapping("/{id}")     // DELETE /api/users/{id}
+    public void delete(@PathVariable Long id){
+        
+    }
+
 
 }
