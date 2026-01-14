24096690be235028703326334b1592372f6beafc
hayoung <hayohio@gmail.com>
2026-01-14 17:12:19 +0900

    Auto: 2026-01-14 17:12

--- jpa-crud/src/main/java/com/example/myapp/controller/UserController.java
+++ jpa-crud/src/main/java/com/example/myapp/controller/UserController.java
@@ -4,6 +4,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController     // JSON 기반 REST 컨트롤러
-@RequestMapping
+@RequestMapping("")
 public class UserController {
 }
