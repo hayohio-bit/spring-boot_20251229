f2f91f841f99b0ee9b600998c3149b5c9c15c905
hayoung <hayohio@gmail.com>
2026-01-14 17:24:35 +0900

    Auto: 2026-01-14 17:24

--- jpa-crud/src/main/java/com/example/myapp/controller/UserController.java
+++ jpa-crud/src/main/java/com/example/myapp/controller/UserController.java
@@ -32,7 +32,7 @@ public class UserController {
     // ==== 3. 회원 수정 ====
     @PostMapping("/{id}")   // POST /api/users/{id}
     public User update(@PathVariable Long id,
-                       @RequestBody User requUser){
+                       @RequestBody User request){
         model.addAttribute(model);
         return "redirect:/api/list";
     }
